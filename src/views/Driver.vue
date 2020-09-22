45bdae7e953e110e3609b4bb928c4aae7a66bc0b
Niall Barber <niall.barber@actual-experience.com>
2020-09-22 13:58:48 +0100

    Added team styles

--- src/views/Driver.vue
+++ src/views/Driver.vue
@@ -1,6 +1,26 @@
 <template>
   <MainLayout>
     <h2>{{ driver.name }}</h2>
+    <p>Born: {{ driver.born }}</p>
+    <p v-if="driver.died">Died: {{ driver.died }}</p>
+    <p>Country: {{ driver.country }}</p>
+    <div class="teams">
+      <span>Teams:</span>
+      <span class="team" v-for="team in driver.teams" :key="team">{{
+        team
+      }}</span>
+    </div>
+    <p>Poles: {{ driver.poles }}</p>
+    <p>Wins: {{ driver.wins }}</p>
+    <div class="championships">
+      <span>Championships: </span>
+      <span
+        class="championship"
+        v-for="championship in driver.championships"
+        :key="championship"
+        >{{ championship }}</span
+      >
+    </div>
   </MainLayout>
 </template>
 
@@ -36,4 +56,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.team {
+  background: #0a2540;
+  margin: 0.5rem;
+  padding: 0.4rem 0.6rem;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: 500;
+}
+
+.championship {
+  @extend .team;
+}
+</style>
