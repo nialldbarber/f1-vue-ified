508e5682864916bcfa7530117c9d2424df27308d
Niall Barber <niall.barber@actual-experience.com>
2020-09-23 08:03:33 +0100

    Added driver id]

--- svelte/src/views/Driver.svelte
+++ svelte/src/views/Driver.svelte
@@ -1,8 +1,14 @@
 <script>
-	let params;
+	import { getContext } from "svelte";
+	import { ROUTER } from "svelte-routing/src/contexts";
+	const { activeRoute } = getContext(ROUTER);
+	const uri = $activeRoute.uri;
+	const driverId = uri.substr(uri.lastIndexOf("/") + 1);
+
+	console.log(driverId);
 </script>
 
 <style>
 </style>
 
-<p>{params.id}</p>
+<p>Hello</p>
